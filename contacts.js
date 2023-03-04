69829ce6dc330f94b1b5e3ae37c72d9bc98a48d1
Maksymilian Bilski <biger9@o2.pl>
2023-03-04 12:08:31 +0100

    'first'

---
+++ contacts.js
@@ -0,0 +1,42 @@
+const fs = require("fs").promises;
+const path = require("path");
+const contacts = require("./db/contacts.json");
+const contactsPath = path.dirname("./db/contacts.json");
+
+const addContact = async (id, name, email, phone) => {
+  const newContact = { id, name, email, phone };
+  const updatedContacts = contacts;
+  updatedContacts.push(newContact);
+  await fs.writeFile(
+    contactsPath + "/contacts.json",
+    JSON.stringify(updatedContacts)
+  );
+};
+
+const removeContact = async (id) => {
+  const contacts = await listContacts();
+  const parsedContacts = JSON.parse(contacts);
+  const tabIndex = parsedContacts.findIndex((el) => el.id === id.toString());
+  parsedContacts.splice(tabIndex, 1);
+  await fs.writeFile(
+    contactsPath + "/contacts.json",
+    JSON.stringify(parsedContacts)
+  );
+};
+
+const getContactById = async (id) => {
+  const contacts = await listContacts();
+  const searchedContact = JSON.parse(contacts).find((el) => {
+    return el.id === id.toString();
+  });
+  console.log(searchedContact);
+  return searchedContact;
+};
+
+const listContacts = async () => {
+  const response = await fs.readFile(contactsPath + "/contacts.json");
+  console.log(response.toString());
+  return response.toString();
+};
+
+module.exports = { addContact, listContacts, getContactById, removeContact };
---
+++ index.js
@@ -0,0 +1,36 @@
+const argv = require("yargs").argv;
+const contacts = require("./contacts");
+console.log(argv);
+
+const { addContact, listContacts, getContactById, removeContact } = contacts;
+
+const usedID = Array.from(listContacts()).map((el) => el.id);
+
+const randomID = () => {
+  const id = Math.random() * 100;
+  if (usedID.includes(id.toFixed(0))) {
+    id = Math.random() * 100;
+  }
+  return id.toFixed(0);
+};
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "add":
+      addContact({ id, name, email, phone });
+      break;
+    case "remove":
+      removeContact(id);
+      break;
+    case "get":
+      getContactById(id);
+      break;
+    case "list":
+      listContacts();
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
